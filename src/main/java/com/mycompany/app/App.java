8acc381f9cf9874bfa2f6ce256aee7e6ad5e20b9
JemimaGra <jemimagrace313@gmail.com>
2024-08-21 16:37:34 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello Jemima";
+    private static final String MESSAGE = "Hello Jemima Grace";
 
     public App() {}
 
